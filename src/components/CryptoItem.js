34cdf9e13d8662ea0b5ee667749a97874b8cb658
Berkay Akbas <berkayakbas@gmail.com>
2022-08-31 17:35:37 +0300

    Create details page and styles

--- src/components/CryptoItem.js
+++ src/components/CryptoItem.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 /* eslint-disable */
 const CryptoItem = (props) => {
@@ -18,18 +18,20 @@ const CryptoItem = (props) => {
   /* eslint-enable */
   return (
     <div className="coin-item">
-      <Link to={`/${id}`} state={{ id: `${id}` }}>
-        <FontAwesomeIcon icon={faCircleArrowRight} className="circle-arrow" />
-      </Link>
-      <img src={image} alt={name} className="coin-image" />
-      <div className="coin-info">
-        <div className="coin-name">{name}</div>
-        <div className="coin-price">
-          {current_price}
-          {' '}
-          $
+      <Link
+        to={`/${id}`}
+        state={{ id: `${id}` }}
+        style={{ textDecoration: "none" }}
+      >
+        <div className="info-holder">
+          <FontAwesomeIcon icon={faCircleArrowRight} className="circle-arrow" />
+          <img src={image} alt={name} className="coin-image" />
+          <div className="coin-info">
+            <div className="coin-name">{name}</div>
+            <div className="coin-price">{current_price} $</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,74 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import CryptoItem from "./CryptoItem";
+import "../styles/Details.css";
+import NumberFormat from "react-number-format";
 
 const Details = () => {
   const { state } = useLocation();
-  /* eslint-disable-next-line */
   const coins = useSelector((state) => state.coins);
+  const coin = coins.filter((coin) => coin.id === state.id);
+  console.log(coin[0].price_change_percentage_24h);
   return (
     <>
-      <div>{state.id}</div>
+      <CryptoItem
+        key={coin[0].id}
+        id={coin[0].id}
+        name={coin[0].name}
+        image={coin[0].image}
+        current_price={coin[0].current_price}
+        market_cap={coin[0].market_cap}
+        market_cap_rank={coin[0].market_cap_rank}
+        total_volume={coin[0].total_volume}
+        price_change_percentage_24h={coin[0].price_change_percentage_24h}
+      />
+      <div className="details-stats">Stats</div>
+      <div className="stat">
+        <div className="stat-name">Market Cap Rank</div>
+        <div className="stat-value">{coin[0].market_cap_rank}</div>
+      </div>
+      <div className="stat">
+        <div className="stat-name">Market Cap</div>
+        <div className="stat-value">
+          <NumberFormat
+            value={coin[0].market_cap}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+          />
+        </div>
+      </div>
+      <div className="stat">
+        <div className="stat-name">Total Volume</div>
+        <div className="stat-value">
+          <NumberFormat
+            value={coin[0].total_volume}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+          />
+        </div>
+      </div>
+      <div className="stat">
+        <div className="stat-name">Price Change (24h)</div>
+
+        <div
+          className="stat-value"
+          style={{
+            color:
+              Math.sign(coin[0].price_change_percentage_24h) < 0
+                ? "red"
+                : "green",
+          }}
+        >
+          <NumberFormat
+            suffix={"%"}
+            displayType={"text"}
+            value={coin[0].price_change_percentage_24h.toFixed(2)}
+          />
+        </div>
+      </div>
     </>
   );
 };
