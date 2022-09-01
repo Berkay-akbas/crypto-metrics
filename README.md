bb7b429ef8797d7eff7e4775ddd3adb63be6c114
Berkay Akbas <berkayakbas@gmail.com>
2022-09-01 05:43:32 +0300

    Fix linter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Space Travelers Hub
 
-> A web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions using SpaceX API. Built with React and Redux.
+> A single page web application that list cryptocurrencies by the market cap. User can click on the coin to get more detailed stats. Built with React and Redux.
 
 ## Built With
 
@@ -72,3 +72,5 @@ Give a ⭐️ if you like this project!
 ## 📝 License
 
 This project is [MIT](./MIT.md) licensed.
+
+
--- src/tests/Details.test.js
+++ src/tests/Details.test.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from '../redux/configureStore';
-import CryptoList from '../components/Cryptolist';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../redux/configureStore";
+/* eslint-disable-next-line */
+import CryptoList from "../components/Cryptolist";
 
-test('fetches & receives a mission after the api call', async () => {
+test("fetches & receives a mission after the api call", async () => {
   render(
     <Provider store={store}>
       <BrowserRouter>
         <CryptoList />
       </BrowserRouter>
-    </Provider>,
+    </Provider>
   );
 
   expect(await screen.findAllByText(/Bitcoin/i)).toHaveLength(5);
 });
 
-test('Shows the data after going into details component', async () => {
+test("Shows the data after going into details component", async () => {
   render(
     <Provider store={store}>
       <BrowserRouter>
         <CryptoList />
       </BrowserRouter>
-    </Provider>,
+    </Provider>
   );
 
   expect(await screen.findByText(/tether gold/i)).toBeInTheDocument();
